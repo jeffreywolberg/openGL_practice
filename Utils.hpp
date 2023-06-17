582198f77c82c54974552aa82641cd7e196acfa2
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-06-16 19:44:40 -0700

    First code snippets

---
+++ Utils.hpp
@@ -0,0 +1,25 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
+
+class Utils {
+public: 
+    static void printShaderLog(GLuint shader);
+    static void printProgramLog(int prog);
+    static bool checkOpenGLError();
+    static string readShaderSource(const char *filePath);
+    static GLuint createShaderProgram(const char *vsource, const char *fsource);
+};
+
+#endif
