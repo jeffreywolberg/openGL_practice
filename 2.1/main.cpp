2488c906bd048b788a5478e4998e93e933a64a35
Jeffrey Wolberg <jeffrey.wolberg@gmail.com>
2023-06-16 23:01:45 -0400

    Works on Linux

--- 2.1/main.cpp
+++ 2.1/main.cpp
@@ -12,49 +12,29 @@ void display(GLFWwindow *window, double currentTime)
 }
 int main(void)
 {
-    cout << "1" << endl;
     if (!glfwInit())
     {
         exit(EXIT_FAILURE);
     }
-    cout << "2" << endl;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    cout << "3" << endl;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    cout << "4" << endl;
     GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
-    cout << "5" << endl;
     glfwMakeContextCurrent(window);
-    cout << "6" << endl;
-#ifndef __APPLE__
     if (glewInit() != GLEW_OK)
     {
-        cout << "6.5" << endl;
         exit(EXIT_FAILURE);
     }
-#endif
-#ifdef __APPLE__
-cout << "Trying some apple config!" << endl;
 glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#endif
-    cout << "7" << endl;
     glfwSwapInterval(1);
-    cout << "8" << endl;
     init(window);
-    cout << "9" << endl;
     while (!glfwWindowShouldClose(window))
     {
-    cout << "9.25" << endl;
         display(window, glfwGetTime());
-    cout << "9.5" << endl;
         glfwSwapBuffers(window);
-    cout << "9.75" << endl;
         glfwPollEvents();
     }
-    cout << "10" << endl;
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
-    cout << "11" << endl;
 }
